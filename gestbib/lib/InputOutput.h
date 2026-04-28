11d722c03381a7e4a0790ee4691df608233afeee
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2026-04-28 13:13:11 +0000

    0.1.2

---
+++ gestbib/lib/InputOutput.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void caricaLibriCSV(Libro* libri, int *libri_size);
+void caricaUtentiCSV(Utente* utenti, int *utenti_size);
+void caricaPrestitiCSV(Prestito** prestiti, int *prestiti_size);
+
+// Carica dati dai file CSV e popola i vettori di libri, utenti e prestiti
+
+void caricaLibriCSV(Libro* libri, int *libri_size);
+void caricaUtentiCSV(Utente* utenti, int *utenti_size);
+void caricaPrestitiCSV(Prestito** prestiti, int *prestiti_size);
--- gestbib/lib/books.h
+++ gestbib/lib/books.h
@@ -7,7 +7,7 @@ typedef struct {
     char *autore;
     char *genere;
     int copie;
-} Libro;
+}Libro;
 
 void aggiungiLibro(Libro* libri, int *libri_size);
 void modificaLibro(Libro* libri, int *libri_size);
@@ -18,31 +18,23 @@ Libro* cercaLibro(int id, Libro* libri, int libri_size);
 // Libri è il puntatore al vettore di libri
 // libr_size è il puntatore al numero di elementi attuali nel vettore
 
-void aggiungiLibro(Libro* libri, int *libri_size) {
-    // Implementazione da completare
-}
+void aggiungiLibro(Libro* libri, int *libri_size);
 
 // Modifica un libro esistente
 // Libri è il puntatore al vettore di libri
 // libr_size è il puntatore al numero di elementi attuali nel vettore
 
-void modificaLibro(Libro* libri, int *libri_size) {
-    // Implementazione da completare
-}
+void modificaLibro(Libro* libri, int *libri_size);
 
 // Elimina un libro dalla lista
 // Libri è il puntatore al vettore di libri
 // libr_size è il puntatore al numero di elementi attuali nel vettore
 
-void eliminaLibro(Libro* libri, int *libri_size) {
-    // Implementazione da completare
-}
+void eliminaLibro(Libro* libri, int *libri_size);
 
 // Cerca un libro nella lista per ID
 // Libri è il puntatore al vettore di libri
 // libr_size è il numero di elementi attuali nel vettore
 // Ritorna NULL se non trovato, altrimenti il puntatore al libro
 
-Libro* cercaLibro(int id, Libro* libri, int libri_size) {
-    // Implementazione da completare
-}
+Libro* cercaLibro(int id, Libro* libri, int libri_size);
